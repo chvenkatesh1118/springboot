55e6afaf12575cc94e63b55158047bd299883ed3
Chanti <chvenkatesh.1118@gmail.com>
2022-12-31 19:10:11 +0530

    22

---
+++ src/main/java/com/rakesh/springboot/WebController.java
@@ -0,0 +1,12 @@
+package com.rakesh.springboot;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+class WebController{
+
+    @RequestMapping("/")
+    public String home(){
+        return "home";
+    }
+}
